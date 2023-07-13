012d67b24e08c29c2590fec2fb13198c2e0c8865
jeong1219 <dst04021@gmail.com>
2023-07-13 18:09:32 +0900

    비주얼 영역 html, css, js 거의 완성. (개인적)

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ if(scy > 0){
     mbt.classList.add("active");
 }
 window.addEventListener("scroll", function(){
-    scy = window.document.documentElement.scrollTop;
+    scy = this.document.documentElement.scrollTop;
     if(scy > 0){
         header.classList.add("active"); // 클래스를 추가함.
         mbt.classList.add("active"); // 스크롤이 됐을 때, 흰색으로 보임.
@@ -47,5 +47,80 @@ mbt.addEventListener("click", function(){
             header.classList.remove("active");
             mbt.classList.remove("active");
         }
+    }else{
+        this.classList.add("ani");
+        htmlRoot.classList.add("active");
+        navMb.classList.add("active");
+        header.classList.add("active");
+        mbt.classList.add("active");
     }
-})
+});
+
+// 반응형 처리
+let winW = window.innerWidth;
+window.addEventListener("resize", function(){
+    winW = window.innerWidth;
+    if(winW > 1024){
+        mbt.classList.remove("ani");
+        htmlRoot.classList.remove("active"); // 없으니 모바일 메뉴가 나타난채로 창을 키웠을 때 스크롤 사라짐
+        navMb.classList.remove("active");
+        if(scy > 0){
+            header.classList.add("active");
+            mbt.classList.add("active");
+        }else{
+            header.classList.remove("active");
+            mbt.classList.remove("active");
+        }
+    }
+});
+
+
+
+
+// 비주얼 슬라이드
+
+var slides = document.querySelectorAll("#slide_box > img");
+var texts = document.querySelectorAll("#title > .text");
+
+var prevBtn = document.getElementById("prevBtn");
+var nextBtn = document.getElementById("nextBtn");
+var current = 0;
+prevBtn.onclick = prevButton; // 괄호?
+nextBtn.onclick = nextButton;
+
+
+function showSlide(n){
+    for(var i = 0 ; i < slides.length ; i++){
+        slides[i].style.display = "none"; // i를 넣는 이유?
+    }
+    slides[n].style.display = "block";
+}
+
+function showText(n){
+    for(var i = 0 ; i < texts.length ; i++){
+        texts[i].style.display = "none";
+    }
+    texts[n].style.display = "block";
+}
+
+function prevButton(){
+    if(current>0){
+        current -= 1;
+    }else{
+        current = slides.length -1;
+    }
+    showSlide(current);
+    showText(current);
+}
+
+function nextButton(){
+    if(current<slides.length -1){
+        current += 1;
+    }else{
+        current = 0;
+    }
+    showSlide(current);
+    showText(current);
+}
+
+var slideInteval = setInterval(nextButton, 3000); // 위치?
