cfb3eecb63d64c9ad4c2262f908949cdb4e49614
jueun3521 <jueun1004juju@gamil.com>
2023-07-17 17:45:35 +0900

    서비스 부분 진행중

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
+// AOS 셋팅
+AOS.init();
+
 // 상단 스크롤 기능
+
 
 const header = document.querySelector("header");
 const mbt = document.querySelector(".mbt");
@@ -75,52 +79,71 @@ window.addEventListener("resize", function(){
 });
 
 
-
-
 // 비주얼 슬라이드
 
-var slides = document.querySelectorAll("#slide_box > img");
-var texts = document.querySelectorAll("#title > .text");
+// 1.슬라이드 (.swiper-slide)개수 만큼 li생성하기
+const swSlideCount = document.querySelectorAll(".sw-visual .swiper-slide").length;
 
-var prevBtn = document.getElementById("prevBtn");
-var nextBtn = document.getElementById("nextBtn");
-var current = 0;
-prevBtn.onclick = prevButton; // 괄호?
-nextBtn.onclick = nextButton;
+// 2.li태그 출력 장소(ul태그 )저장
+const swSlidePgUl = document.querySelector(".sw-visual-pg-list");
 
-
-function showSlide(n){
-    for(var i = 0 ; i < slides.length ; i++){
-        slides[i].style.display = "none"; // i를 넣는 이유?
-    }
-    slides[n].style.display = "block";
+//3.li에 html로 작성할 글자를 생성한다.
+let swVisualHtml = ``;
+for (let i = 0; i < swSlideCount; i++) {
+    swVisualHtml = swVisualHtml + `<li>${i + 1}</li>`;
 }
 
-function showText(n){
-    for(var i = 0 ; i < texts.length ; i++){
-        texts[i].style.display = "none";
-    }
-    texts[n].style.display = "block";
-}
+// 4.html을 추가한다
+swSlidePgUl.innerHTML = swVisualHtml;
 
-function prevButton(){
-    if(current>0){
-        current -= 1;
-    }else{
-        current = slides.length -1;
-    }
-    showSlide(current);
-    showText(current);
-}
+// 5.페이지네이션 관련(코딩으로 생성한 li태그 저장)
+const swVisualPgLi = document.querySelectorAll(".sw-visual-pg-list > li");
+console.log(swVisualPgLi);
 
-function nextButton(){
-    if(current<slides.length -1){
-        current += 1;
-    }else{
-        current = 0;
-    }
-    showSlide(current);
-    showText(current);
-}
 
-var slideInteval = setInterval(nextButton, 3000); // 위치?
+var swiper = new Swiper(".sw-visual", {
+    effect: "fade",
+    // fadeEffect: {
+    //   crossFade: true,
+    // },
+    loop: true,
+    // 슬라이드의 모션 속도를 transition 맞춘다.
+    speed: 1000,
+    autoplay: {
+        delay: 3000,
+        // 사용자가 마우스 클릭 드래그로 이동하면
+        // 아래 구문이 없으면 autoplya 가 해제되므로
+        // 이것을 방지해 주기위한 처리
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".sw-visual-next",
+        prevEl: ".sw-visual-prev",
+    },
+});
+
+
+// swiper 가 최초 실행 될 때
+// 1번 li 의 흰색 라인이 늘어나는 모션을 실행
+swVisualPgLi[0].classList.add("active");
+// swiper가 바뀔때 마다 실행
+// 슬라이더가 바뀌는 상태를 찾아서
+// 우리가 적용하고자 하는 처리를 하고
+// swiper의 API를 참조해서 작성
+
+swiper.on("slideChange", function () {
+    swVisualPgLi.forEach(function (item, index) {
+        if (swiper.realIndex === index) { // realIndex는 진짜 html태그의 순서값
+            item.classList.add("active");
+        }else {
+            item.classList.remove("active");
+        }
+    });
+});
+
+// li 태그를 클릭했을 때
+swVisualPgLi.forEach(function(item,index){
+    item.addEventListener("click", function(){
+        swiper.slideToLoop(index, 500, false);
+    });
+});
